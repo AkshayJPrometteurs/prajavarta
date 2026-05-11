e28898c664bf280aaf4c4b9c5631ec8f144d8ce2
AkshayJ <akshay.j@prometteur.in>
2026-05-11 16:07:24 +0530

    remove loader

--- src/app/loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="min-h-full flex flex-col items-center justify-center">
-            <h1>Please Wait...</h1>
-        </div>
-    )
-}
